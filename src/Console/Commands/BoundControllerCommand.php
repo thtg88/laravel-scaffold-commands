51a4f9b8bf2790e27716dd2d4bba470d40a23f23
Marco Marassi <thtg88@users.noreply.github.com>
2020-06-14 22:18:39 +0100

    Added bound controller stub and command

---
+++ src/Console/Commands/BoundControllerCommand.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Thtg88\LaravelScaffoldCommands\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputArgument;
+
+class BoundControllerCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scaffold:bound-controller {name : The name of the class}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new controller class with bindings';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Controller';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/bound-controller.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Http\Controllers';
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param string  $name
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        $stub = $this->files->get($this->getStub());
+
+        return $this->replaceNamespace($stub, $name)
+            // ->replaceRepository($stub, $name)
+            ->replaceClass($stub, $name);
+    }
+
+    /**
+     * Replace the model class name for the given stub.
+     *
+     * @param string  $stub
+     * @param string  $name
+     * @return string
+     */
+    // protected function replaceRepository(&$stub, $name)
+    // {
+    //     $class = str_replace($this->getNamespace($name).'\\', '', $name);
+    //
+    //     $class = str_replace('Service', 'Repository', $class);
+    //
+    //     $stub = str_replace('DummyRepository', $class, $stub);
+    //
+    //     return $this;
+    // }
+}
